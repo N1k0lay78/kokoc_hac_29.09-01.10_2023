f331acea4a8ceb7d71ce401b80de587d7e3201ab
Rjkzavr <kolya.toropof@gmail.com>
2023-09-30 19:14:43 +0300

    add chart

---
+++ static/js/chart/elements/index.js
@@ -0,0 +1,4 @@
+export {default as ArcElement} from './element.arc.ts';
+export {default as LineElement} from './element.line.js';
+export {default as PointElement} from './element.point.ts';
+export {default as BarElement} from './element.bar.js';
