39ee6c46b8523f8a70d0d1ae3d1ad75688358194
Rjkzavr <kolya.toropof@gmail.com>
2023-09-30 20:46:49 +0300

    make coast

--- main.py
+++ main.py
@@ -13,5 +13,12 @@ def log_page():
     return render_template("log-form.html")
 
 
+@application.route("/ui1/<int:coast>")
+def ui_page(coast):
+    if coast == 2:
+        coast = -1
+    return render_template("UI-kit.html", coast_motion=int(coast))
+
+
 if __name__ == '__main__':
     application.run("192.168.1.109")
