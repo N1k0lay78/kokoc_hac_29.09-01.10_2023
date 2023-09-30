258fd8eb497b344586a155f03f7064002c00f92a
Rjkzavr <kolya.toropof@gmail.com>
2023-09-30 19:15:05 +0300

    make base design

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def main_page():
     return render_template("base.html")
 
 
+@application.route("/log/")
+def log_page():
+    return render_template("log-form.html")
+
+
 if __name__ == '__main__':
-    application.run()
+    application.run("192.168.1.109")
