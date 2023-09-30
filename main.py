8a809bcaf4bed21ed7248c45b6f5e27db421ec2f
Rjkzavr <kolya.toropof@gmail.com>
2023-10-01 02:16:08 +0300

    make my render and macros

--- main.py
+++ main.py
@@ -5,26 +5,43 @@ application = Flask(__name__)
 db_session.global_init("db/kokos.sqlite")
 
 
+def my_render(filename, **kwargs):
+    my_kwargs = {
+        "need_log": True,
+        "is_authorized": False,  # need irl data
+        "is_logout": False,
+        "user_id": -1,  # need irl data
+    }
+    for key, val in kwargs.items():
+        my_kwargs[key] = val
+    return render_template(filename, **my_kwargs)
+
 @application.route("/")
 def main_page():
-    return render_template("base.html")
+    is_authorized = True
+    return my_render("base.html")
+
+
+@application.route("/login/")
+def login_page():
+    return my_render("base.html", need_log=False)
 
 
 @application.route("/log/")
 def log_page():
-    return render_template("log-form.html")
+    return my_render("log-form.html", need_log=False)
 
 
 @application.route("/ui1/<int:coast>")
 def ui1_page(coast):
     if coast == 2:
         coast = -1
-    return render_template("UI-kit-coast.html", coast_motion=int(coast))
+    return my_render("UI-kit-coast.html", coast_motion=int(coast))
 
 
 @application.route("/ui2/")
 def ui2_page():
-    return render_template("UI-kit-chart.html")
+    return my_render("UI-kit-chart.html")
 
 
 @application.route("/ui3/<int:id>")
@@ -43,7 +60,7 @@ def ui3_page(id):
         ["Cha Cha", 56262.36, 11],
         ["turtle", 56262.36, 12],
     ]
-    return render_template("UI-kit-leaderboard.html", leaderboard=leaderboard, user_id=id)
+    return my_render("UI-kit-leaderboard.html", leaderboard=leaderboard, user_id=id, need_log=True)
 
 
 if __name__ == '__main__':
