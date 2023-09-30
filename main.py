8fe139ea8c38432d21a9a8302a2b8b7a5d5bb8c6
Rjkzavr <kolya.toropof@gmail.com>
2023-10-01 01:02:27 +0300

    make leaderboard

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from data import db_session
 
 application = Flask(__name__)
-db_session.global_init("db/kokos.sqlite")
+# db_session.global_init("db/kokos.sqlite")
 
 
 @application.route("/")
@@ -23,9 +23,28 @@ def ui1_page(coast):
 
 
 @application.route("/ui2/")
-def ui_page():
+def ui2_page():
     return render_template("UI-kit-chart.html")
 
 
+@application.route("/ui3/<int:id>")
+def ui3_page(id):
+    leaderboard = [
+        ["Rjkzavr", 56262.36, 1],
+        ["Nikniksham", 56262.36, 2],
+        ["Niki", 56262.36, 3],
+        ["Juk", 56262.36, 4],
+        ["Rjkz", 56262.36, 5],
+        ["NikTV_78", 56262.36, 6],
+        ["bobr", 56262.36, 7],
+        ["kaiga", 56262.36, 8],
+        ["dragon", 56262.36, 9],
+        ["itv", 56262.36, 10],
+        ["Cha Cha", 56262.36, 11],
+        ["turtle", 56262.36, 12],
+    ]
+    return render_template("UI-kit-leaderboard.html", leaderboard=leaderboard, user_id=id)
+
+
 if __name__ == '__main__':
-    application.run()
+    application.run("192.168.1.109")
